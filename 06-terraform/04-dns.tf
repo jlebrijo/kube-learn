8e406f942ff1b77a465a7b98b4eaf56cca4deee1
Juan Lebrijo <juan@lebrijo.com>
2019-10-16 13:03:27 +0200

    minor

--- 06-terraform/04-dns.tf
+++ 06-terraform/04-dns.tf
@@ -1,7 +1,13 @@
+// Comment for existing domains
+//resource "digitalocean_domain" "lebrijo" {
+//  name = "lebrijo.com"
+//}
+
+# Add a record to the domain
 resource "digitalocean_record" "www" {
   domain = "lebrijo.com"
   type   = "A"
-  name   = "test2"
+  name   = "test"
   ttl    = "40"
   value  = "${digitalocean_loadbalancer.public.ip}"
 }
