4d10200366d35f07e07b2a8b7a6697e4f22aac60
Juan Lebrijo <juan@lebrijo.com>
2019-11-04 18:13:33 +0100

    minor

--- 07-persistent-volumes/00-terraform.tf
+++ 07-persistent-volumes/00-terraform.tf
@@ -9,7 +9,7 @@ resource "digitalocean_kubernetes_cluster" "mycluster" {
   name    = "mycluster"
   region  = "ams3"
   // Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.15.4-do.0"
+  version = "1.15.5-do.0"
 
   node_pool {
     name       = "mypool"
